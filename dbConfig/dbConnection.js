5743ec6b4f4e43f353f0029182811a825dd6c6cc
Manish Gupta <guptamns3786@gmail.com>
2023-07-15 14:31:01 +0530

    second commit

--- dbConfig/dbConnection.js
+++ dbConfig/dbConnection.js
@@ -2,10 +2,8 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/demoApi', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        // await mongoose.connect('mongodb://127.0.0.1:27017/demoApi', {
+        await mongoose.connect('mongodb+srv://guptamns3786:NfwpgTOs6M1Mse1e@cluster0.wcgo120.mongodb.net/demoApi');
         console.log("successfully connect to mongodb database");
     } catch (error) {
         console.log(error.message);
