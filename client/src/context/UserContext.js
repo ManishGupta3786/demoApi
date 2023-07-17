a6407b317140a1bac716e2187bdeb455dedfad7a
Manish Gupta <guptamns3786@gmail.com>
2023-07-17 13:48:27 +0530

    sixth commit

--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -5,19 +5,22 @@ export const UserContext = createContext();
 export const UserContextProvider = ({children})=>{
     const [user, setUser] = useState(null);
 
+    // const baseUrl = "http://localhost:5000/demoApi";
+    const baseUrl = "http://localhost:5000/demoApi";
+
     useEffect(()=>{
         let userData = JSON.parse(localStorage.getItem("user"));
         setUser(userData);
     },[])
 
     const saveUser = async (data) =>{
-        const response = await axios.post('http://184.72.152.2/demoApi/register',data);
+        const response = await axios.post(`${baseUrl}/register`,data);
         localStorage.setItem("user",JSON.stringify(response.data.data));
         setUser(response.data.data);
     }
 
     const getUser = async (data) =>{
-        const response = await axios.post('http://184.72.152.2/demoApi/login',data);
+        const response = await axios.post(`${baseUrl}/login`,data);
         localStorage.setItem("user",JSON.stringify(response.data));
         setUser(response.data);
     }
